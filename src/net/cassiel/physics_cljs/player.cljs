74e34d90266c8b55d755d432cfd75d285f4ba425
Nick Rothwell <nick@cassiel.com>
2016-10-13 22:06:31 +0100

    Property clean-up.

--- src/net/cassiel/physics_cljs/player.cljs
+++ src/net/cassiel/physics_cljs/player.cljs
@@ -1,27 +1,33 @@
 (ns net.cassiel.physics-cljs.player
   (:require [net.cassiel.physics-cljs.matter-core :as m]
             [net.cassiel.physics-cljs.protocols :as px]
-            [quil.core :as q :include-macros true])
-  )
+            [quil.core :as q :include-macros true]))
+
+(def PROPS "PROPS")
+
+(defn swap-property!
+  "`f` probably `assoc`, `update` etc."
+  [js-obj f]
+  (aset js-obj PROPS (f (aget js-obj PROPS))))
 
 (defn remember-size [body]
   (let [min (-> body .-bounds .-min)
         max (-> body .-bounds .-max)]
-    (set! (.-_size body)
-          #js [(- (.-x max) (.-x min))
-               (- (.-y max) (.-y min))])
+    (swap-property! body #(assoc % :size [(- (.-x max) (.-x min))
+                                          (- (.-y max) (.-y min))]))
     body))
 
 (defn box [& {:keys [position size colour opts]}]
   (let [[x y] position
         [w h] size
-        body (-> (.rectangle m/BODIES x y w h (clj->js opts)) remember-size)]
+        body (-> (.rectangle m/BODIES x y w h (clj->js opts))
+                 remember-size)]
     (reify px/PLAYER
       (draw [_]
         (q/with-translation [(.. body -position -x)
                              (.. body -position -y)]
           (q/with-rotation [(.. body -angle)]
-            (let [[w h] (.. body -_size)]
+            (let [[w h] (:size (aget body PROPS))]
               (apply q/fill (or colour [255 255 255]))
               (q/rect 0 0 w h)))))
 
@@ -29,12 +35,13 @@
 
 (defn disc [& {:keys [position radius colour opts]}]
   (let [[x y] position
-        body (-> (.circle m/BODIES x y radius (clj->js opts)) remember-size)]
+        body (-> (.circle m/BODIES x y radius (clj->js opts))
+                 remember-size)]
     (reify px/PLAYER
       (draw [_]
         (q/with-translation [(.. body -position -x)
                              (.. body -position -y)]
-          (let [[w h] (.. body -_size)]
+          (let [[w h] (:size (aget body PROPS))]
             (apply q/fill (or colour [255 255 255]))
             (q/ellipse 0 0 w h))))
 
